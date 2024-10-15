745c26efc1a33044de88decf9adb5d00a9091190
Masum Gökyüz <masumgokyuz@gmail.com>
2024-10-15 22:05:56 +0300

    db cleanup on signal interrupt

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
 import { App } from "./app";
 import { Controllers } from "@/controllers";
+import { logger } from "@/utils/logger";
 
 const app = new App(Controllers);
-app.listen();
+
+app.listen().catch((error) => {
+    logger.error("Failed to start the server", error);
+    process.exit(1);
+});
+
+process.on("SIGINT", async () => {
+    await app.closeDatabase();
+    process.exit(0);
+});
