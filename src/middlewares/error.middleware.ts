cbc9ec531f2274083636f02fe77742d96b9afc00
Masum Gökyüz <masumgokyuz@gmail.com>
2024-10-15 17:52:32 +0300

    refactor

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,8 +4,8 @@ import { logger } from "@/utils/logger";
 
 export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
     try {
-        const status: number = error.status || 500;
-        const message: string = error.message || "Something went wrong";
+        const status = error.status ?? 500;
+        const message = error.message ?? "Something went wrong";
 
         logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
         res.status(status).json({ message });
